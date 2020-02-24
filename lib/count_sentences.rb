6bb998bb16a4f8bdf9dc37fc99d1ad95b3bc8fb4
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-24 23:53:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ class String
     if self.sentence? || self.question? || self.exclamation?
       self.split
     end
-    self.split.delete_if{|sentence| sentence.size < 2}.size
+    self.split.delete_if{|s| s.size < 2}.size
   end
   
 end
